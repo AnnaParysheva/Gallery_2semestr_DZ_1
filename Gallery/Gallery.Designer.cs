a4e25fd3ff72e38109ab9806f8c29c389fa225fb
AnnaParysheva <parysevaanna36@gmail.com>
2025-03-18 20:20:12 +0300

    Add files via upload

---
+++ Gallery/Gallery.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Gallery
+{
+    partial class Gallery
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxPictures = new System.Windows.Forms.ListBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.txtBoxListPictures = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxPictures
+            // 
+            this.listBoxPictures.FormattingEnabled = true;
+            this.listBoxPictures.ItemHeight = 16;
+            this.listBoxPictures.Location = new System.Drawing.Point(372, 40);
+            this.listBoxPictures.Name = "listBoxPictures";
+            this.listBoxPictures.Size = new System.Drawing.Size(416, 292);
+            this.listBoxPictures.TabIndex = 0;
+            this.listBoxPictures.SelectedIndexChanged += new System.EventHandler(this.listBoxPictures_SelectedIndexChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 40);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(350, 361);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(350, 22);
+            this.textBoxTitle.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(372, 338);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(152, 36);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 407);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(162, 36);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // txtBoxListPictures
+            // 
+            this.txtBoxListPictures.Location = new System.Drawing.Point(372, 12);
+            this.txtBoxListPictures.Name = "txtBoxListPictures";
+            this.txtBoxListPictures.Size = new System.Drawing.Size(416, 22);
+            this.txtBoxListPictures.TabIndex = 5;
+            this.txtBoxListPictures.Text = "Список изображений:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBoxListPictures);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.listBoxPictures);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Галерея";
+            this.Text = "Галерея";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxPictures;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox txtBoxListPictures;
+    }
+}
+
---
+++ Gallery/Gallery.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Gallery
+{
+    public partial class Gallery : Form
+    {
+        public Gallery()
+        {
+            InitializeComponent();
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                listBoxPictures.Items.Clear();
+                pictureBox.Image = null;
+                textBoxTitle.Clear();
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                foreach (string line in lines)
+                {
+                    listBoxPictures.Items.Add(line);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                List<string> lines = new List<string>();
+                foreach (var item in listBoxPictures.Items)
+                {
+                    string line = item.ToString();
+                    if (item == listBoxPictures.SelectedItem && !string.IsNullOrEmpty(textBoxTitle.Text))
+                    {
+                        string[] parts = line.Split(',');
+                        line = parts[0] + "," + textBoxTitle.Text;
+                    }
+                    lines.Add(line);
+                }
+                File.WriteAllLines(saveFileDialog.FileName, lines);
+            }
+        }
+
+        private void listBoxPictures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxPictures.SelectedItem != null)
+            {
+                string[] parts = listBoxPictures.SelectedItem.ToString().Split(',');
+                if (File.Exists(parts[0]))
+                {
+                    pictureBox.Image = Image.FromFile(parts[0]);
+                }
+                if (parts.Length > 1)
+                {
+                    textBoxTitle.Text = parts[1];
+                }
+                else
+                {
+                    textBoxTitle.Text = string.Empty;
+                }
+            }
+        } 
+    }
+}
